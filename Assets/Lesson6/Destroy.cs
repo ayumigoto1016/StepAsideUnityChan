a0a25224f4dd190f3fdd0a3a2d3ff0d2e43da58d
ayumi goto <ayumi.goto.1016@gmail.com>
2019-10-06 17:20:54 +0900

    Lesson6 発展課題
    動的にアイテムを生成できるようにしました。

--- Assets/Lesson6/Destroy.cs
+++ Assets/Lesson6/Destroy.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class Destroy : MonoBehaviour {
 
-	void OnBecameInvisible(){
+	private GameObject maincamera;
+
+
+
+	/*void OnBecameInvisible(){
 
 		Destroy (this.gameObject);
 
-	}
+	}*/
 
 	// Use this for initialization
 	void Start () {
-		
+
+		this.maincamera = Camera.main.gameObject;
+
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (this.transform.position.z < maincamera.transform.position.z) {
+
+			Destroy (this.gameObject);
 		
-	}
+		}
+}
 }
 
--- Assets/Lesson6/ItemGenerator.cs
+++ Assets/Lesson6/ItemGenerator.cs
@@ -6,39 +6,64 @@ public class ItemGenerator : MonoBehaviour {
 	public GameObject carPrefab;
 	public GameObject coinPrefab;
 	public GameObject conePrefab;
+
+	public GameObject unitychan;
+
+
 	private int startPos = -160;
 	private int goalPos =120;
 	private float posRange = 3.4f;
 
+	private float difference; //最後にアイテム生成した位置から進んだ距離
+
+	public float generatePosZ; //アイテム生成時のunitychanのZ座標
+
+
+
 	// Use this for initialization
 	void Start () {
 
-		for(int i = startPos; i < goalPos; i+= 15){
-			int num = Random.Range (1,11);
-			if(num <= 2){
-				for(float j = -1; j <= 1; j += 0.4f){
-					GameObject cone = Instantiate (conePrefab) as GameObject;
-					cone.transform.position = new Vector3 (4 * j, cone.transform.position.y, i);
-				}
+		this.unitychan = GameObject.Find ("unitychan"); //unitychanの情報取得
+		this.generatePosZ = -240;
+
+		}
+
+	void Update () {
+
+		this.difference = this.unitychan.transform.position.z - generatePosZ;
+
+
+		if( difference > 15 ){
+
+
+				int num = Random.Range (1,11);
+				if(num <= 2){
+					for(float j = -1; j <= 1; j += 0.4f){
+						GameObject cone = Instantiate (conePrefab) as GameObject;
+					cone.transform.position = new Vector3 (4 * j, cone.transform.position.y, unitychan.transform.position.z + 45);
+					}
 				}else{
 					for(int j = -1; j <= 1; j++){
 						int item = Random.Range(1,11);
 						int offsetZ = Random.Range(-5,6);
 						if(1 <= item && item <=6){
 							GameObject coin = Instantiate (coinPrefab) as GameObject;
-							coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, i+offsetZ);
+						coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, unitychan.transform.position.z + 45 + offsetZ);
 						}else if(7 <= item && item <= 9){
 							GameObject car = Instantiate (carPrefab) as GameObject;
-							car.transform.position = new Vector3(posRange * j , car.transform.position.y, i+offsetZ);
-		}
-		
+						car.transform.position = new Vector3(posRange * j , car.transform.position.y, unitychan.transform.position.z + 45 + offsetZ);
+						}
+
+					    }
+		             }
+			                generatePosZ = unitychan.transform.position.z;//アイテム生成時の座標を更新
+		       }
+
 	}
-				}
-			}
-		}
-	
-	// Update is called once per frame
-	void Update () {
+
+
 		
-	}
+			
+
+
 }
